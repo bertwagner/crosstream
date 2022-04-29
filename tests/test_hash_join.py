d535c41b0a8be25622ad0f5cc1464f0f299d6651
Bert Wagner <bertwagner@bertwagner.com>
2022-04-28 20:43:41 -0400

    init test file

---
+++ tests/test_hash_join.py
@@ -0,0 +1,11 @@
+import pytest
+
+class TestHashJoin:
+
+    def test_one(self):
+        self.value = 1
+        assert self.value == 1
+
+    def test_two(self):
+        self.value = 1
+        assert self.value == 2
