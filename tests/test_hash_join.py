39dac06d76620bb37fd860debd670a33c0ce5cf3
Bert Wagner <bertwagner@bertwagner.com>
2022-05-10 20:57:41 -0400

    add custom override tests

--- tests/test_hash_join.py
+++ tests/test_hash_join.py
@@ -98,9 +98,50 @@ d,1,2,d
 d,2,2,d
 '''
 
+def test_custom_overrides(tmp_path_factory,csv_input_data,sqlite_input_data):
 
-# odbc to odbc
-# hash join
-# loop join
-# custom override functions
-# output more values in override functions
+    # columns referred to by index and name
+    c1=CSVData(csv_input_data[0],True,[0,1])
+    q1=QueryData(sqlite_input_data,'SELECT * from test_data',['col1','col2'])
+
+    # define a function for joining on criteria that is modified before insert into hash table
+    def custom_join_key(row,indices):
+        # calculate the hash of join values
+        join_values = []
+        for col_index in indices:
+            join_values.append(str(row[col_index]))
+        join_key = '2020-01-01|'.join(join_values)
+
+        return join_key
+
+    # define a function for determining if a matched join key is truly a match, allowing additional output columns
+    def custom_process_matched_hashes(bucket_row,probe_row, bucket_join_column_indexes, probe_join_column_indexes):
+        weight=1.0
+        return tuple(bucket_row),tuple(probe_row),(weight,)
+
+    h=HashJoin()
+
+    output_file = os.path.join(tmp_path_factory.getbasetemp(),'csv_to_odbc_overrides_output.csv')
+
+    with open(output_file, 'w') as f:
+        w =csv.writer(f)
+        
+        # write header column names
+        w.writerow(c1.column_names + q1.column_names + ['weight'])
+
+        for row_left,row_right,weight in h.inner_join(c1,q1,custom_join_key,custom_process_matched_hashes):
+            # write matched results
+            w.writerow(row_left + row_right + weight)
+
+    with open(output_file, 'r') as f:
+        file_content = f.read()
+        print(file_content)
+        assert file_content == '''col1,col2,col1,col2,weight
+a,1,a,1,1.0
+a,3,a,3,1.0
+b,1,b,1,1.0
+c,3,c,3,1.0
+'''
+
+
+
